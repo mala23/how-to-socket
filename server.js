19f75fb7b5fc10620b1ba1bf1ef47c89cd0ea8d0
mala23 <mail@mala23.me>
2014-10-28 23:46:26 +0000

    add socket.io

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var server = require('express')();
 var http = require('http').Server(server);
+var io = require('socket.io')(http);
 // server.engine('jade', require('jade').__express);
 
 server.get('/', function(req, res){
   res.render('index.jade');
+});
+
+io.on('connection', function(){
+  console.log('a user connected');
 });
 
 server.listen(1337, function(){
