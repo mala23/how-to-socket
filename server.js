e40a91bd1f10d5e1fbdd547fd942cba9e6efa97b
mala23 <mail@mala23.me>
2014-10-30 15:58:09 +0000

    socket does console.log on connect, want to move this into client side script later

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-var server = require('express')();
+var express = require('express');
+var server = express();
 var http = require('http').Server(server);
 var io = require('socket.io')(http);
-// server.engine('jade', require('jade').__express);
+
+server.use(express.static(__dirname + '/public'));
 
 server.get('/', function(req, res){
   res.render('index.jade');
 });
 
-io.on('connection', function(){
+io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-server.listen(1337, function(){
+http.listen(1337, function(){
   console.log('listening on *:1337');
 });
